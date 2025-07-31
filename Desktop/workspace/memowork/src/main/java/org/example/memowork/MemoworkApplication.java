c15f940e4f8d37204b4b305090ac7887efb08f56
baek_do_hyeon <ehgus6666@naver.com>
2025-07-31 22:25:57 +0900

    first commit

---
+++ Desktop/workspace/memowork/src/main/java/org/example/memowork/MemoworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.memowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoworkApplication.class, args);
+	}
+
+}
---
+++ Desktop/workspace/memowork/src/test/java/org/example/memowork/MemoworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.memowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
